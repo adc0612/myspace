ddb7c054f2f9173e690fc3b10bcd2b14ac8f82a3
Dohu <adc0612@gmail.com>
2019-09-10 02:51:02 +0900

    20190910 1st commit

--- learn-vue-js/vue-cli/src/App.vue
+++ learn-vue-js/vue-cli/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <app-header></app-header>
+    <!-- <app-header v-bind:프롭스 속성이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <!-- 하위레벨 vue에서 emit을 하면 그 emit된 function은 하위 컴포넌트를 binding하고 있는 태그에 온다 -->
+    <!-- 그것을 v-on:"하위컴포넌트 Function"을 통해 받는다. 
+    이 하위 컴포넌트에서 emit을 하면 실행할 function을 methods안에 function을 선언한다.
+     -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
@@ -9,11 +14,16 @@ import AppHeader from './components/AppHeader.vue';
 export default {
   data: function() {
     return  {
-      str: 'hi hello'
+      str: 'Header'
     }
   },
   components: {
     'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function(){
+      this.str = 'hi';
+    }
   }
 }
 </script>
--- learn-vue-js/vue-cli/src/components/AppHeader.vue
+++ learn-vue-js/vue-cli/src/components/AppHeader.vue
@@ -1,12 +1,21 @@
 <template>
   <header>
-      <h1>Header</h1>
+      <!-- 밑에 propsdata를 {{propsdata}}를 쓰면 상위레벨에 있는 내려오는 데이터를 넣을 수 있다. -->
+      <h1>{{propsdata}}</h1>
+      <button v-on:click="sendEvent">add</button>
   </header>
 </template>
 
 <script>
 export default {
-
+  //props를 선언 하면 상위 레벨의 이거를 써줄 테그에 v-bind로 propsdata를 엮어주면
+  //상위의 data를 밑으로 내려 쓸 수 있다.
+  props: ['propsdata'],
+  methods: {
+    sendEvent: function(){
+      this.$emit('renew')
+    }
+  }
 }
 </script>
 
